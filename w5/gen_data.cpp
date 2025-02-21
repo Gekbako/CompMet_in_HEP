60299e2f73052743440f83d94d83c48e0f73997d
Jakub <jakub.gregus@gmail.com>
2025-02-21 04:22:01 +0200

    hw5

---
+++ w5/gen_data.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <TFile.h>
+#include <TTree.h>
+#include <TRandom3.h>
+
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    char datafname[20] = "data.root";
+    TFile *file = new TFile(datafname, "RECREATE");
+
+    TTree *tree = new TTree("tree", "Tree with normally distributed data");
+
+    double value;
+
+    // branch of a tree
+    tree->Branch("value", &value, "value/D");
+
+    TRandom3 randGen(42);
+
+    // generate 1000 normal dist. numbers
+    for (int i = 0; i < 1000; i++) {
+        value = randGen.Gaus(0, 1);
+        tree->Fill();
+    }
+
+    tree->Write();
+    file->Close();
+    
+    cout << "Data written to file: "<<datafname<<endl;
+    return 0;
+}
